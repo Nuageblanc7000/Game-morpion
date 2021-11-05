ef6f9bd6514b70c51cf8a3120416c175bd1fce43
remy <wetterene.remy@gmail.com>
2021-11-05 20:57:47 +0100

    morpion v.1

---
+++ public/javascript/javascript.js
@@ -0,0 +1,264 @@
+
+//mais deux svg sont stocké dans des variables pour simplifier ('avoir si il n'y a pas une meilleur)
+let svgO=`<svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_101:3)">
+    <path d="M47.375 9.8125C47.125 10.1875 46.9792 10.6042 46.9375 11.0625C46.8958 11.5208 46.875 11.9375 46.875 12.3125C46.875 12.8125 46.875 13.3125 46.875 13.8125C46.9167 14.2708 46.9375 14.7292 46.9375 15.1875V16.125C46.8542 16.4167 46.7292 16.7083 46.5625 17C46.3958 17.2917 46.2708 17.5833 46.1875 17.875C45.6042 19.4167 45.3125 20.9583 45.3125 22.5V22.625C45.1042 23.125 44.8958 23.5833 44.6875 24C44.5208 24.4167 44.3542 24.8333 44.1875 25.25C44.0208 25.625 43.8958 26.0417 43.8125 26.5C43.7292 26.9167 43.7083 27.4167 43.75 28C43.1667 29.4583 42.3958 30.7917 41.4375 32C40.4792 33.1667 39.5417 34.375 38.625 35.625C37.75 35.9583 36.9375 36.3958 36.1875 36.9375C35.4792 37.4792 34.7917 38.0833 34.125 38.75C32.8333 39.1667 31.5625 39.6458 30.3125 40.1875C29.0625 40.7292 27.9375 41.4583 26.9375 42.375C26.1458 42.4583 25.3542 42.5833 24.5625 42.75C23.7708 42.9167 23 43.1667 22.25 43.5H21.5C21.125 43.5 20.8542 43.4167 20.6875 43.25C20.1458 43.5417 19.5833 43.75 19 43.875C18.4167 43.9583 17.8333 44 17.25 44C16.5 44 15.75 43.9583 15 43.875C14.2917 43.75 13.5625 43.625 12.8125 43.5C12.3958 42.9167 11.8125 42.4792 11.0625 42.1875C10.3125 41.8542 9.64583 41.5417 9.0625 41.25C7.85417 39.5833 6.97917 37.9792 6.4375 36.4375C5.89583 34.8542 5.33333 33.0833 4.75 31.125C4.83333 30.5417 4.97917 29.9792 5.1875 29.4375C5.39583 28.8542 5.47917 28.2292 5.4375 27.5625C6.1875 25.3958 7.16667 23.4375 8.375 21.6875C9.625 19.8958 11.0417 18.1875 12.625 16.5625C12.625 16.3125 12.6667 16.1042 12.75 15.9375C12.875 15.7708 12.8958 15.5208 12.8125 15.1875C12.8542 15.2292 12.9375 15.25 13.0625 15.25C13.3958 15.25 13.6042 15.1667 13.6875 15C13.7708 14.7917 13.7917 14.5625 13.75 14.3125C15.25 13.3542 16.6042 12.25 17.8125 11C19.0625 9.75 20.4792 8.70833 22.0625 7.875C22.1875 7.5 22.3958 7.1875 22.6875 6.9375C22.9792 6.6875 23.2083 6.39583 23.375 6.0625C24.7083 5.6875 25.9167 5.125 27 4.375C28.0833 3.625 29.2292 2.95833 30.4375 2.375C31.1042 2 31.7292 1.75 32.3125 1.625C32.9375 1.45833 33.6042 1.29167 34.3125 1.125C34.5625 1.04167 34.7917 0.979167 35 0.9375C35.2083 0.895833 35.4375 0.875 35.6875 0.875C36.4375 0.875 37.1667 1.02083 37.875 1.3125C38.5833 1.5625 39.3125 1.6875 40.0625 1.6875C40.2708 1.6875 40.4375 1.6875 40.5625 1.6875C40.7292 1.64583 40.8958 1.60417 41.0625 1.5625C41.7708 2.4375 42.6667 3.10417 43.75 3.5625C44 4.1875 44.2917 4.72917 44.625 5.1875C45 5.64583 45.375 6.10417 45.75 6.5625C46.125 6.97917 46.4583 7.45833 46.75 8C47.0833 8.5 47.2917 9.10417 47.375 9.8125ZM41.125 13.0625C41.125 12.8542 41.1042 12.6667 41.0625 12.5C40.6458 12.375 40.25 12.2083 39.875 12C39.5 11.7917 39.0833 11.6875 38.625 11.6875C38.3333 11.6875 38.0417 11.75 37.75 11.875C37.7083 11.5833 37.5833 11.375 37.375 11.25C37.2083 11.125 37 11.0208 36.75 10.9375C36.5417 10.8125 36.3125 10.7292 36.0625 10.6875C35.8542 10.6042 35.6667 10.4792 35.5 10.3125C35.375 9.97917 35.2292 9.64583 35.0625 9.3125C34.9375 8.9375 34.6875 8.66667 34.3125 8.5C33.9375 8.875 33.5 9.1875 33 9.4375C32.5 9.64583 31.9792 9.85417 31.4375 10.0625C30.9375 10.2708 30.4375 10.5208 29.9375 10.8125C29.4792 11.1042 29.0833 11.4583 28.75 11.875H27.625C27.375 12.3333 27.0208 12.6875 26.5625 12.9375C26.1042 13.1458 25.7083 13.4583 25.375 13.875C24.7083 14.2083 24.0833 14.625 23.5 15.125C22.9583 15.5833 22.4167 16.0833 21.875 16.625C21.3333 17.125 20.7917 17.6458 20.25 18.1875C19.75 18.7292 19.2083 19.2292 18.625 19.6875C18.0833 20.8542 17.4792 21.875 16.8125 22.75C16.1875 23.5833 15.5833 24.4375 15 25.3125C14.4583 26.1875 13.9792 27.1458 13.5625 28.1875C13.1875 29.1875 13 30.4375 13 31.9375V32.5625C13 32.7708 13.0208 32.9792 13.0625 33.1875C13.6042 33.7708 14.0417 34.3958 14.375 35.0625C14.75 35.7292 15.3542 36.2083 16.1875 36.5C17.0208 36.8333 17.8958 36.9792 18.8125 36.9375C19.7708 36.8958 20.6875 37 21.5625 37.25C23.5625 36.875 25.4792 36.2708 27.3125 35.4375C29.1875 34.5625 30.9583 33.5208 32.625 32.3125C32.875 32.1042 33.1667 31.8958 33.5 31.6875C33.8333 31.4375 34.1042 31.1875 34.3125 30.9375C34.5208 30.7292 34.6875 30.5 34.8125 30.25C34.9792 30 35.1458 29.7708 35.3125 29.5625C35.6458 29.0208 36.0208 28.4792 36.4375 27.9375C36.8542 27.3958 37.2917 26.8958 37.75 26.4375C37.9583 25.5208 38.2292 24.6458 38.5625 23.8125C38.8958 22.9375 39.2292 22.0833 39.5625 21.25C39.8958 20.4167 40.1667 19.5625 40.375 18.6875C40.625 17.8125 40.75 16.8958 40.75 15.9375C40.75 15.7292 40.7292 15.5 40.6875 15.25C40.6875 15 40.6667 14.7708 40.625 14.5625C40.9583 14.1042 41.125 13.6042 41.125 13.0625Z" fill="#FF0000"/>
+    </g>
+    <defs>
+    <filter id="filter0_d_101:3" x="0.75" y="0.875" width="50.625" height="51.125" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset dy="4"/>
+    <feGaussianBlur stdDeviation="2"/>
+    <feComposite in2="hardAlpha" operator="out"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_101:3"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_101:3" result="shape"/>
+    </filter>
+    </defs>
+    </svg>
+    `
+    let svgX=`<svg width="53" height="60" viewBox="0 0 53 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_101:3)">
+        <path d="M48.375 2.4375C48.2917 3.3125 47.9792 3.97917 47.4375 4.4375C46.9375 4.85417 46.3542 5.29167 45.6875 5.75C44.8125 7.04167 43.875 8.29167 42.875 9.5C41.9167 10.7083 40.9167 11.9167 39.875 13.125C38.875 14.2917 37.875 15.5 36.875 16.75C35.9167 17.9583 35.0417 19.2292 34.25 20.5625C33.2917 21.2708 32.4375 22.0625 31.6875 22.9375C30.9375 23.7708 30.2083 24.6458 29.5 25.5625C28.7917 26.4375 28.0833 27.3542 27.375 28.3125C26.7083 29.2292 26 30.1042 25.25 30.9375C25.5 31.9375 25.9375 32.7292 26.5625 33.3125C27.1875 33.8958 27.875 34.4375 28.625 34.9375C29.375 35.3958 30.1042 35.8958 30.8125 36.4375C31.5208 36.9792 32.0625 37.6875 32.4375 38.5625C33.6875 38.7708 34.8958 39.1042 36.0625 39.5625C37.2708 39.9792 38.5208 40.1875 39.8125 40.1875C40.7708 40.1875 41.6875 39.9375 42.5625 39.4375C42.9375 39.5208 43.3125 39.6667 43.6875 39.875C44.1042 40.0833 44.5417 40.1875 45 40.1875C45.375 40.1875 45.75 40 46.125 39.625C46.2083 39.75 46.2917 39.9167 46.375 40.125C46.4583 40.2917 46.6042 40.375 46.8125 40.375C46.8958 40.375 46.9583 40.375 47 40.375C47.0833 40.3333 47.1458 40.3125 47.1875 40.3125V41.3125C47.1875 41.6875 47.2083 42.0625 47.25 42.4375C47.2917 42.7708 47.375 43.125 47.5 43.5C47.1667 43.5 46.8958 43.5625 46.6875 43.6875L46.125 44.0625C45.9583 44.1875 45.75 44.2917 45.5 44.375C45.25 44.4583 44.9167 44.4583 44.5 44.375C44.25 44.4167 44.0833 44.5417 44 44.75C43.9583 44.9583 43.8333 45.125 43.625 45.25C43.3333 45.4583 42.9375 45.5625 42.4375 45.5625C41.9792 45.5625 41.5625 45.6667 41.1875 45.875L39.0625 46C38.3542 46.0417 37.6458 46.0625 36.9375 46.0625C36.5208 46.0625 36.0208 46.0625 35.4375 46.0625C34.8542 46.0208 34.2708 45.9792 33.6875 45.9375C33.1042 45.8542 32.5417 45.7708 32 45.6875C31.4583 45.5625 31 45.4167 30.625 45.25C29.25 44.5833 27.9167 43.75 26.625 42.75C25.3333 41.75 24.0833 40.7708 22.875 39.8125L21.875 38.9375C21.5417 38.6458 21.1875 38.375 20.8125 38.125C20.7708 38.0833 20.6667 38.0625 20.5 38.0625C20.2917 38.0625 20.1042 38.1042 19.9375 38.1875C19.8125 38.2708 19.6667 38.3125 19.5 38.3125C19.3333 38.3125 19.25 38.25 19.25 38.125C17.9583 39.375 16.75 40.7083 15.625 42.125L12.25 46.375C12.25 46.7083 12.3333 46.9792 12.5 47.1875L13 47.75C13.1667 47.9167 13.3125 48.1042 13.4375 48.3125C13.6042 48.5208 13.6875 48.7917 13.6875 49.125C13.6875 49.375 13.6667 49.5833 13.625 49.75C13.25 49.875 12.8125 49.9583 12.3125 50C11.8125 50.0417 11.2917 50.1042 10.75 50.1875C10.25 50.3125 9.79167 50.4583 9.375 50.625C8.95833 50.8333 8.66667 51.1458 8.5 51.5625C8.20833 51.4792 7.9375 51.3958 7.6875 51.3125C7.47917 51.2292 7.20833 51.2083 6.875 51.25C6.54167 50.8333 6.20833 50.4167 5.875 50C5.5 49.5417 5.16667 49.0625 4.875 48.5625C4.83333 48.2708 4.8125 48 4.8125 47.75C4.77083 47.4583 4.75 47.1667 4.75 46.875C4.75 45.9167 4.89583 45.0625 5.1875 44.3125C5.47917 43.5208 5.8125 42.7083 6.1875 41.875C6.89583 41.5417 7.54167 41.1875 8.125 40.8125C8.75 40.3958 9.16667 39.7917 9.375 39C9.91667 38.9583 10.3542 38.7917 10.6875 38.5C11.0625 38.1667 11.3958 37.8125 11.6875 37.4375C12.0208 37.0625 12.3542 36.7292 12.6875 36.4375C13.0208 36.1458 13.4792 35.9583 14.0625 35.875C14.0625 35.7917 14.0417 35.7083 14 35.625C14 35.5417 14 35.4583 14 35.375C14 35.0833 14.0625 34.875 14.1875 34.75C14.3125 34.5833 14.4583 34.4583 14.625 34.375C14.8333 34.25 15.0208 34.1458 15.1875 34.0625C15.3958 33.9792 15.5625 33.8333 15.6875 33.625C15.5208 33.1667 15.3333 32.7292 15.125 32.3125C14.9583 31.8958 14.75 31.5 14.5 31.125C14.0833 30.5 13.6458 29.8958 13.1875 29.3125L11.8125 27.5625C11.6042 27.2708 11.3333 27.0208 11 26.8125C10.7083 26.5625 10.4583 26.2708 10.25 25.9375C10.0417 25.6042 9.91667 25.2292 9.875 24.8125C9.83333 24.3958 9.72917 24.0208 9.5625 23.6875C9.35417 23.2708 9.08333 22.8958 8.75 22.5625C8.45833 22.1875 8.3125 21.7292 8.3125 21.1875C8.3125 20.8958 8.375 20.625 8.5 20.375C8.04167 19.9167 7.70833 19.375 7.5 18.75C7.29167 18.0833 7.1875 17.4375 7.1875 16.8125C7.1875 16.2292 7.27083 15.6458 7.4375 15.0625C7.60417 14.4375 7.875 13.875 8.25 13.375H8.8125C9.02083 13.375 9.125 13.3125 9.125 13.1875C9.375 13.3542 9.64583 13.5 9.9375 13.625C10.2708 13.75 10.4583 13.9792 10.5 14.3125C10.875 14.3542 11.25 14.4167 11.625 14.5L12.75 14.75C13.4167 15.75 14.0208 16.7708 14.5625 17.8125C15.1042 18.8542 15.6458 19.8958 16.1875 20.9375C16.7708 21.9792 17.375 23 18 24C18.625 25 19.3333 25.9375 20.125 26.8125C20.75 26.5625 21.2917 26.1875 21.75 25.6875C22.2083 25.1875 22.625 24.6667 23 24.125C23.4167 23.5833 23.8542 23.0625 24.3125 22.5625C24.7708 22.0625 25.3125 21.6875 25.9375 21.4375C25.8958 21.3958 25.875 21.3333 25.875 21.25C25.875 21.0417 25.9375 20.8958 26.0625 20.8125C26.2292 20.6875 26.2708 20.4583 26.1875 20.125C27.0208 19.5417 27.7917 18.9167 28.5 18.25C29.2083 17.5833 29.7083 16.7083 30 15.625H30.875C31.875 14.2917 32.875 12.9583 33.875 11.625C34.9167 10.2917 36.0833 9.08333 37.375 8V7.125C38.0417 6.70833 38.625 6.22917 39.125 5.6875C39.625 5.10417 40.1042 4.52083 40.5625 3.9375C41.0208 3.3125 41.4583 2.6875 41.875 2.0625C42.3333 1.4375 42.8542 0.875 43.4375 0.375C43.8542 0.583333 44.2917 0.729167 44.75 0.8125C45.2083 0.895833 45.6458 1 46.0625 1.125C46.5208 1.25 46.9375 1.41667 47.3125 1.625C47.7292 1.79167 48.0833 2.0625 48.375 2.4375Z" fill="#FF0000"/>
+        </g>
+        <defs>
+        <filter id="filter0_d_101:3" x="0.75" y="0.375" width="51.625" height="59.1875" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="out"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_101:3"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_101:3" result="shape"/>
+        </filter>
+        </defs>
+        </svg>
+        `
+//gestion du formulaire
+const inscription = document.querySelector('.inscription')
+const forms = document.querySelector('.forms')
+const inputs = forms.querySelectorAll('input')
+const button = forms.querySelector('button')
+let actifGame = false;
+let tabName = []
+const player1 = {
+name: '',
+score: 0,
+time: null,
+play: true
+}
+const player2 = {
+name: '',
+score: 0,
+time: null,
+play: true
+}
+// function de verif des inputs
+const verifInput = (input, e) => {
+e.preventDefault();
+if (!input.value || inputs.item(0).value == inputs.item(1).value) {
+input.classList.add('border-danger', 'border-4')
+} else if (inputs.item(0).value && inputs.item(1).value) {
+//on vide si jamais on restart la game
+player1.name = ''
+player2.name = ''
+//on push
+tabName.push(input.value)
+//et on nomme nos joueurs
+player1.name = tabName[0]
+player2.name = tabName[1]
+inscription.classList.add('d-none')
+return actifGame = true
+} else {
+input.classList.remove('border-danger', 'border-4')
+input.classList.add('border-success', 'border-4')
+}
+}
+//event qui va permettre l'insertion de mes joueurs et vérifier aussi si le form d'inscription est bien rempli
+button.addEventListener('click', (e) => {
+inputs.forEach(input => {
+verifInput(input, e)
+})
+//on récup toutes les cases.
+const containerCase = document.querySelector('#container-game');
+const cases = containerCase.querySelectorAll('.case')
+
+//recup des div score
+const score = document.querySelectorAll('.score')
+//recup du status
+const status = document.querySelector('#status')
+//mes buttons reset et newsGame
+const menuGame = document.querySelector('.menu')
+const btns = menuGame.querySelectorAll('button')
+// mon tab recap pour voir ou j'en suis dans la partie
+let recapTab = []
+
+if (actifGame == true) {
+//function status
+const statuss = () => {
+ // on va afficher à qui est le tour.
+ if (player1.play == true) {
+     score.item(0).innerHTML = player1.score
+     return status.innerHTML = 'joueur 1:' + ' ' + player1.name
+ } else {
+    score.item(1).innerHTML = player2.score
+     return status.innerHTML = 'joueur 2:' + ' ' + player2.name
+ }
+}
+
+const activMenu = (actifGame) => {
+ if (actifGame == false) {
+     btns.forEach((btn, key) => {
+         btn.classList.remove('disabled')
+         if (key == 0) {
+             btn.addEventListener('click', restart)
+         } else if(key==1){
+             btn.addEventListener('click', newGames)
+         }
+     })
+ } else {
+     btn.classList.add('disabled')
+ }
+}
+statuss()
+cases.forEach((casse, key) => {
+ //casse.innerHTML=key
+ casse.addEventListener('click', (e) => {
+     if (casse.innerHTML == "" && actifGame == true) {
+         //vérification du joueur en cours
+         if (player1.play == true) {
+             casse.dataset.l='X'
+             casse.innerHTML = svgX
+             casse.classList.add('animation')
+             player1.play = false
+             player2.play = true
+         } else if (player2.play == true) {
+            casse.dataset.l='O'
+            casse.innerHTML = svgO
+            casse.classList.add('animationO')
+             player2.play = false
+             player1.play = true
+         }
+         //permet de vérifier si mon tableau est rempli
+         recapTab.push(casse.innerHTML)
+         //horizontal
+         if (cases.item(0).dataset.l == "X" && cases.item(1).dataset.l == "X" && cases.item(2).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(0).dataset.l == "O" && cases.item(1).dataset.l == "O" && cases.item(2).dataset.l == "O") {
+             player2.score = +1
+         }
+         if (cases.item(3).dataset.l == "X" && cases.item(4).dataset.l == "X" && cases.item(5).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(3).dataset.l == "O" && cases.item(4).dataset.l == "O" && cases.item(5).dataset.l == "O") {
+             player2.score = +1
+         }
+         if (cases.item(6).dataset.l == "X" && cases.item(7).dataset.l == "X" && cases.item(8).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(6).dataset.l == "O" && cases.item(7).dataset.l == "O" && cases.item(8).dataset.l == "O") {
+             player2.score = +1
+         }
+         //end Horiz
+         //vertical
+         if (cases.item(0).dataset.l == "X" && cases.item(3).dataset.l == "X" && cases.item(6).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(0).dataset.l == "O" && cases.item(3).dataset.l == "O" && cases.item(6).dataset.l == "O") {
+             player2.score = +1
+         }
+         if (cases.item(1).dataset.l == "X" && cases.item(4).dataset.l == "X" && cases.item(7).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(1).dataset.l == "O" && cases.item(4).dataset.l == "O" && cases.item(7).dataset.l == "O") {
+             player2.score = +1
+         }
+         if (cases.item(2).dataset.l == "X" && cases.item(5).dataset.l == "X" && cases.item(8).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(2).dataset.l == "O" && cases.item(5).dataset.l == "O" && cases.item(8).dataset.l == "O") {
+             player2.score = +1
+         }
+         // end vertical
+         if (cases.item(0).dataset.l == "X" && cases.item(4).dataset.l == "X" && cases.item(8).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(0).dataset.l == "O" && cases.item(4).dataset.l == "O" && cases.item(8).dataset.l == "O") {
+             player2.score = +1
+         }
+         if (cases.item(2).dataset.l == "X" && cases.item(4).dataset.l == "X" && cases.item(6).dataset.l == "X") {
+             player1.score = +1
+         } else if (cases.item(2).dataset.l == "O" && cases.item(4).dataset.l == "O" && cases.item(6).dataset.l == "O") {
+             player2.score = +1
+         }
+
+         // on relance le status ici pour l'actualiser après chaque click
+         statuss()
+         //on vérifie si la game n'est pas win
+         winGame()
+
+     }
+ })
+ const winGame = () => {
+    if (player1.score == 1) {
+        sendMessage(player1.name)
+        actifGame = false
+        activMenu(actifGame)
+        return actifGame;
+    }
+    if (player2.score == 1) {
+        sendMessage(player2.name)
+        actifGame = false
+        activMenu(actifGame)
+        return actifGame;
+    }
+    if (recapTab.length === cases.length) {
+        sendMessage(player1.name,player2.name) 
+        actifGame = false
+        activMenu(actifGame)
+        return actifGame;
+    } else {
+        // je gère le statut du joueur ici comme ça si il est gagnant son nom ne changera pas au dernier click
+        return game = true
+    }
+}
+})
+//function simple restart sans effacer les joueurs
+const restart = () => {
+ // on vide le tableau des récap
+ //on vide les casses
+ player1.score = 0
+ player2.score = 0
+ recapTab.length = 0
+ cases.forEach(voidCasse => {
+     voidCasse.dataset.l=""
+     voidCasse.innerHTML =""
+     voidCasse.classList.remove('animation')
+     voidCasse.classList.remove('animationO')
+ })
+ victoryMess.innerHTML=""
+ //on réactive la game
+ actifGame = true
+}
+//function  nouvelle game avec nouveaux noms de joueurs
+const newGames = () => {
+ restart()
+tabName.length = 0
+ actifGame = false;
+ inscription.classList.remove('d-none')
+ inputs.forEach(input => {
+     input.value = ""
+     verifInput(input, e)
+     input.classList.remove('border-danger', 'border-4')
+ })
+ 
+}
+}
+})
+//function pour envoyer le message
+const victoryMess = document.querySelector('.victoryMess')
+const sendMessage=(player1=null,player2=null)=>{
+let victory =`${player1} à remporté la partie`
+let egality =`égalité entre ${player1} et ${player2} `
+if(player1 !== null && player2==null){   
+  return victoryMess.innerHTML=`${victory}`
+}else{
+  return victoryMess.innerHTML=`${egality}`
+}
+}
